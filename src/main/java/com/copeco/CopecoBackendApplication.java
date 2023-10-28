40ed44dd09a2e4a550d9b6740ea6eae5db757bb4
Omer-Espinoza <ocef20@gmail.com>
2023-10-28 12:54:10 -0500

    [ADD] Create project

---
+++ src/main/java/com/copeco/CopecoBackendApplication.java
@@ -0,0 +1,13 @@
+package com.copeco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CopecoBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CopecoBackendApplication.class, args);
+    }
+
+}
